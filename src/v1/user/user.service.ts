062a03651a7c740556b4a7b30e3d667ad85bbbc1
NgocHai056 <nguyenngochai056@gmail.com>
2023-10-05 21:16:04 +0700

    TÊN:Nguyen Ngoc Hai DỰ ÁN: [CINEMA] NỘI DUNG: update method login MÃ TASK: [] LINK TASK: STORE: [] TABLE: []

--- src/v1/user/user.service.ts
+++ src/v1/user/user.service.ts
@@ -3,6 +3,7 @@ import { User } from './user.entity/user.entity';
 import { Repository } from "typeorm";
 import { InjectRepository } from '@nestjs/typeorm';
 import { BaseService } from 'src/base.service/base.service';
+import { UtilsExceptionMessageCommon } from 'src/utils.common/utils.exception.common/utils.exception.message.common';
 
 @Injectable()
 export class UserService extends BaseService<User>{
@@ -11,5 +12,15 @@ export class UserService extends BaseService<User>{
         private readonly userRepository: Repository<User>
     ) {
         super(userRepository);
+    }
+
+    async checkExisting(email: string): Promise<any> {
+        const user = await this.userRepository.findBy({ email: email });
+
+        if (user.length !== 0) {
+            UtilsExceptionMessageCommon.showMessageError("This account has already existed.");
+        }
+
+        return user;
     }
 }
