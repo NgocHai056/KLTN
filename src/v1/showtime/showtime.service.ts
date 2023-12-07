2ea2cb9711d4beae33fe6ecf0c7f72fb28eb6c92
NgocHai056 <nguyenngochai056@gmail.com>
2023-12-07 11:55:37 +0700

    TÊN:Nguyen Ngoc Hai DỰ ÁN: [CINEMA] NỘI DUNG: auto create showtime MÃ TASK: [] LINK TASK: STORE: [] TABLE: []

--- src/v1/showtime/showtime.service.ts
+++ src/v1/showtime/showtime.service.ts
@@ -306,7 +306,7 @@ export class ShowtimeService extends BaseService<Showtime> {
         return showtimes;
     }
 
-    @Cron(CronExpression.EVERY_DAY_AT_1AM)
+    @Cron(CronExpression.EVERY_DAY_AT_NOON)
     async autoCreateShowtime() {
 
         const nextDate = moment().add(4, 'days');
