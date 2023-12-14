f89fcc6e64db41048594a6314c02f9c453624833
NgocHai056 <nguyenngochai056@gmail.com>
2023-12-14 21:46:23 +0700

    Merge TÊN:Nguyen Ngoc Hai DỰ ÁN: [CINEMA] NỘI DUNG: delete token user when block MÃ TASK: [] LINK TASK: STORE: [] TABLE: []

--- src/v1/user/user.controller.ts
+++ src/v1/user/user.controller.ts
@@ -62,6 +62,7 @@ export class UserController {
         if (!user)
             UtilsExceptionMessageCommon.showMessageError("User not exist.");
 
+        user.access_token = "";
         response.setData(new UserResponse(await this.userService.update(user.id, { status: UserStatus.STOP_WORKING })));
         return res.status(HttpStatus.OK).send(response);
     }
